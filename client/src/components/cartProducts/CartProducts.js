b301eaf05026529ef9cacdba7542536989dad8b8
paraspatle-jpg <paras18224827@gmail.com>
2022-07-01 22:15:43 +0530

    setting up update options

--- client/src/components/cartProducts/CartProducts.js
+++ client/src/components/cartProducts/CartProducts.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./CartProducts.css";
+import { Increase } from "../assets/Increase";
+import { Decrease } from "../assets/Decrease";
+import { Delete } from "../assets/Delete";
 
 export const CartProducts = () => {
   const cartProd = [
@@ -9,16 +12,33 @@ export const CartProducts = () => {
     { name: "Tshirt", quantity: 3 },
   ];
 
+  const updateCount = (count) => {};
+  const deleteFromCart = () => {};
+
   return (
     <div className="cart-products-container">
       <div className="cart-products-flex-container">
         <div className="cart-heading"> Your Cart</div>
-        <hr/>
+        <hr className="cart-heading-hr" />
         {cartProd.map((product) => {
           return (
             <div className="cart-product-flex-container">
               <span>{product.name}</span>
-              <span>{product.quantity}</span>
+              <span>
+                <span className="cart-update-svg" onClick={updateCount(-1)}>
+                  <Decrease></Decrease>
+                </span>
+                <span className="cart-count-text">{product.quantity}</span>
+                <span className="cart-update-svg" onClick={updateCount(1)}>
+                  <Increase></Increase>
+                </span>
+                <span
+                  className="cart-update-svg cart-delete-svg"
+                  onClick={deleteFromCart(1)}
+                >
+                  <Delete></Delete>
+                </span>
+              </span>
             </div>
           );
         })}
